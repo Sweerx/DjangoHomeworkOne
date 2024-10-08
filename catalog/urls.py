ab780dae2680b41a71c91788b247c74929cd63d6
Dmitry Yakushin <yakushin.dima8@gmail.com>
2024-10-08 20:47:09 +0300

    Доп задание выполнено

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
@@ -6,4 +7,11 @@ def home(request):
 
 
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+
+        return HttpResponse('Данные успешно отправлены!')
+    return render(request, 'catalog/contacts.htmp')
